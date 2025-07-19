7d43d8d75e105a914550486db581148c74b11339
benja-amb1 <benjaminrodriguezambroselli@outlook.com.ar>
2025-07-19 20:16:24 -0300

    add: html and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [box, setBox] = useState();
+  const [horizontalPx, setHorizontalPx] = useState(0);
+  const [verticalPx, setVerticalPx] = useState(0);
+  const [blurRadius, setBlurRadius] = useState(0);
+  const [spreadRadius, setSpreadRadius] = useState(0);
+  const [boxColor, setBoxColor] = useState('#000000');
+  const [opacity, setOpacity] = useState(0.75);
+  const [inset, setInset] = useState(false);
+
+  const toggleInset = () => {
+    setInset(prev => !prev)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <div className='box-reference'>
+
+        </div>
+
+        <div className='box-user'>
+          <input type="range" min='-200' max='200' onChange={e => setHorizontalPx(e.target.value)} />
+
+          <p>Horizontal Shadow Length: {horizontalPx}px</p>
+
+          <input type="range" min='-200' max='200' onChange={e => setVerticalPx(e.target.value)} />
+
+          <p>Vertical Shadow Length: {verticalPx}px</p>
+
+
+          <input type="range" min='0' max='400' onChange={e => setBlurRadius(e.target.value)} />
+
+          <p>Blur Radius: {blurRadius}px</p>
+
+
+          <input type="range" min='-200' max='200' onChange={e => setSpreadRadius(e.target.value)} />
+
+          <p>Spread Radius: {spreadRadius}px</p>
+
+          <input type="color" onChange={e => setBoxColor(e.target.value)} />
+          <p>Shadow Color: {boxColor}</p>
+
+          <input type="range" min='0' max='1' onChange={e => setOpacity(e.target.value)} />
+
+          <p>Shadow Color Opacity: {spreadRadius}px</p>
+
+          <input type='checkbox' onClick={toggleInset} />
+          <p>{inset ? 'ON' : 'OFF'}</p>
+
+        </div>
+
+      </section>
     </>
   )
 }
